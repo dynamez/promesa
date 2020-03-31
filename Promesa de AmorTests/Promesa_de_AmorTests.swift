b0085f6bea6a69699db5df855dd96fcbc14e2084
Felipe Valdes Verdugo <fvaldesv@everis.com>
2020-03-30 22:37:41 -0300

    Initial Commit

---
+++ Promesa de AmorTests/Promesa_de_AmorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Promesa_de_AmorTests.swift
+//  Promesa de AmorTests
+//
+//  Created by Felipe Valdes on 30-03-20.
+//  Copyright © 2020 Rayout. All rights reserved.
+//
+
+import XCTest
+@testable import Promesa_de_Amor
+
+class Promesa_de_AmorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
